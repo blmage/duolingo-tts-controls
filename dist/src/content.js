84f8fa47b3aae14003cd715fbe449e88d163a87f
blmage <bl.mage.fr@gmail.com>
2020-07-09 09:42:12 +0200

    Add assets (screenshots)

--- dist/src/content.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var e=document.createElement("script");e.src=chrome.runtime.getURL("src/ui.js"),e.type="text/javascript",(document.head||document.documentElement).appendChild(e);var t=document.createElement("link");t.href=chrome.runtime.getURL("assets/css/ui.css"),t.rel="stylesheet",(document.head||document.documentElement).appendChild(t)}();
