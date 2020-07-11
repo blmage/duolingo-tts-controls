4cb42c674de64681d9f6a79c12f5179577cf51ee
blmage <bl.mage.fr@gmail.com>
2020-07-11 14:43:26 +0200

    Implement a debug-level logging function

--- src/functions.js
+++ src/functions.js
@@ -54,17 +54,35 @@ export function discardEvent(event) {
 let loggingIframe = null;
 
 /**
+ * @private
+ * @returns {Console} A working console object.
+ */
+function getLoggingConsole() {
+  if (!loggingIframe || !loggingIframe.isConnected) {
+    loggingIframe = document.createElement('iframe');
+    loggingIframe.style.display = 'none';
+    document.body.appendChild(loggingIframe);
+  }
+
+  return loggingIframe.contentWindow.console;
+}
+
+/**
+ * @param {*} message The extension-related message/value to log to the console.
+ * @param {?string} prefix A prefix to prepend to the message.
+ */
+export function logDebug(message, prefix = '') {
+  if ('development' === process.env.NODE_ENV) {
+    getLoggingConsole().log(`[${EXTENSION_CODE}] ${prefix}`, message);
+  }
+}
+
+/**
  * @param {*} error The extension-related error to log to the console.
  * @param {?string} prefix A prefix to prepend to the error.
  */
-export function logError(error, prefix) {
+export function logError(error, prefix = '') {
   if ('development' === process.env.NODE_ENV) {
-    if (!loggingIframe || !loggingIframe.isConnected) {
-      loggingIframe = document.createElement('iframe');
-      loggingIframe.style.display = 'none';
-      document.body.appendChild(loggingIframe);
-    }
-
-    loggingIframe.contentWindow.console.error(`[${EXTENSION_CODE}] ${prefix || ''}`, error);
+    getLoggingConsole().error(`[${EXTENSION_CODE}] ${prefix}`, error);
   }
 }
